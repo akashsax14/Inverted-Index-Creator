f9debcea970fb6838dc511c8a042ac5153526adf
Akash Saxena <akarocks14@gmail.com>
2014-03-17 00:35:57 -0400

    Updating write_interim to include comments and debug flags

--- write_interim.py
+++ write_interim.py
@@ -1,29 +1,37 @@
-import os
-from collections import defaultdict
-import zlib
-import base64
-
-def write_interim(interim_index, doc_id):
+#======================================================================#
+# Writes the data of an intermediate index for a single data file into #
+# a file. Called for every data file.                                  #
+# Inverted index is created by deleimiting word and list of doc ids    #
+# by :-: character for future parsing and merging of index files       #
+#======================================================================#
+def write_interim(interim_index, doc_id, debug_flag, compression_flag):
 	try:	
-		index_rw = open('indexed/index_'+ str(doc_id) +'.txt', 'wb+')
-		#index_rwc = open('indexed/index_'+ str(doc_id) +'c.txt', 'wb+')
+		index_rw = open('indexed/index_'+ str(doc_id) +'.txt', 'w+')
 		
+		#Compressing intermediate index by representing list of doc_ids as difference in doc_ids
+		if compression_flag:
+			for k, v in interim_index.iteritems():
+				l = len(v)
+				c = l-1
+				while c >= 1:
+					v[c] = v[c] - v[c-1]
+					c -= 1
+		
+		#Sorting inverted index based on word(dictionary key)
 		sorted_interim_index = sorted(interim_index)
 		
-		#open("temp.txt", 'w+').write(str(sorted_interim_index)+'\n')
+		#Writing the intermediate inverted index for a single data file
 		for k in sorted_interim_index:
-			index_rw.write(k+':-:'+str(interim_index[k])+':-:\n')
-			#index_rw.write(k+' '+str(v)+'\n')
-		index_rw.seek(0)
-		file_content = index_rw.read()
-		compressed_content = base64.b64encode(zlib.compress(file_content))		
+			if compression_flag:
+				#Adding a "|" character for delimiting a section of compression for each data file
+				index_rw.write(k+':-:'+str(interim_index[k]+['|'])+':-:\n')
+			else:
+				index_rw.write(k+':-:'+str(interim_index[k])+':-:\n')
+			
 		index_rw.close()
-		with open('indexed/index_'+ str(doc_id) +'.txt', 'wb+') as fp:
-			fp.write(compressed_content)
-		#os.system('sort -f -o indexed/index_'+str(doc_id)+'.txt indexed/index_'+str(doc_id)+'.txt')
 	except Exception as e:
 		index_rw.close()
-		#index_rwc.close()
-		print "Exception in Write_Interim : " + str(e)
+		if debug_flag:
+			print "Exception in Write_Interim : " + str(e)
 		pass
-		
+#=======================================================================
